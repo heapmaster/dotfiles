d33c174469947e55238774174a6438c692c63a96
snewberry <snewberry@hudsonalpha.org>
2016-06-02 11:29:36 -0500

    Iniital cut at slate.js
    This should be semi-workable for now.

--- slate.js
+++ slate.js
@@ -6,7 +6,7 @@ var rightScreenRef = "1";
 var bottomMiddle = function(screenRef) {
     return S.op("move", {
             "screen": screenRef,
-            "x": "screenOriginX + (screenSizeX / 2)",
+            "x": "screenOriginX + (screenSizeX / 4)",
             "y": "screenOriginY + (screenSizeY / 2)",
             "width": "screenSizeX / 2",
             "height": "screenSizeY / 2"
@@ -26,7 +26,7 @@ var bottomLeft = function(screenRef) {
 var bottomRight = function(screenRef) {
     return S.op("move", {
             "screen": screenRef,
-            "x": "screenOriginX + (screenSizeX / 3 * 2)",
+            "x": "screenOriginX + (screenSizeX / 2)",
             "y": "screenOriginY + (screenSizeY / 2)",
             "width": "screenSizeX / 2",
             "height": "screenSizeY / 2"
@@ -36,7 +36,7 @@ var bottomRight = function(screenRef) {
 var topMiddle = function(screenRef) {
     return S.op("move", {
             "screen": screenRef,
-            "x": "screenOriginX + (screenSizeX / 2)",
+            "x": "screenOriginX + (screenSizeX / 4)",
             "y": "screenOriginY",
             "width": "screenSizeX / 2",
             "height": "screenSizeY / 2"
@@ -56,31 +56,72 @@ var topLeft = function(screenRef) {
 var topRight = function(screenRef) {
     return S.op("move", {
             "screen": screenRef,
-            "x": "screenOriginX + (screenSizeX / 3 * 2)",
+            "x": "screenOriginX + (screenSizeX / 2)",
             "y": "screenOriginY",
             "width": "screenSizeX / 2",
             "height": "screenSizeY / 2"
             });
 };
 
+var middleMiddle = function(screenRef) {
+    return S.op("move", {
+            "screen": screenRef,
+            "x": "screenOriginX + (screenSizeX / 4)",
+            "y": "screenOriginY + (screenSizeY / 4)",
+            "width": "screenSizeX / 2",
+            "height": "screenSizeY / 2"
+            });
+};
+
+var bigMiddle = function(screenRef) {
+    return S.op("move", {
+            "screen": screenRef,
+            "x": "screenOriginX + (screenSizeX / 8)",
+            "y": "screenOriginY + (screenSizeY / 10)",
+            "width": "screenSizeX * 4 / 5",
+            "height": "screenSizeY * 4 / 5"
+    });
+};
+
 var oneMonitorLayout = S.layout("oneMonitor", {
-    "iTerm": {
-        "operations": [ bottomLeft(leftScreenRef), bottomMiddle(leftScreenRef), bottomRight(leftScreenRef) ],
+    "iTerm2": {
+        "operations": [ topLeft(leftScreenRef), topRight(leftScreenRef), bottomLeft(leftScreenRef), bottomRight(leftScreenRef) ],
         "sort-title": true,
         "repeat": true
     },
     "Google Chrome": {
-        "operations": [ topMiddle(leftScreenRef) ],
+        "operations": [ bigMiddle(leftScreenRef) ],
         "repeat": true,
         "ignore-fail": true
+    },
+    "Slack": {
+        "operations": [ bottomLeft(leftScreenRef) ]
     }
 });
 
-var twoMonitorLayout = S.layout("twoMonitor", {});
+var twoMonitorLayout = S.layout("twoMonitor", {
+    "iTerm2": {
+        "operations": [ topLeft(leftScreenRef), topRight(leftScreenRef), bottomLeft(leftScreenRef), bottomRight(leftScreenRef) ],
+        "sort-title": true,
+        "repeat": true
+    },
+    "Google Chrome": {
+        "operations": [ bigMiddle(leftScreenRef), bigMiddle(rightScreenRef) ],
+        "ignore-fail": true,
+        "repeat": true
+    },
+    "Slack": {
+        "operations": [ topLeft(rightScreenRef) ]
+    },
+    "Spotify": {
+        "operations": [ bottomLeft(rightScreenRef) ]
+    }
+});
 
-S.bind("o:cmd,shift", S.op("layout", { "name": oneMonitorLayout }));
+S.bind("o:cmd,shift", S.op("layout", { "name": twoMonitorLayout }));
 
 S.default(1, oneMonitorLayout);
+S.default(["3840x2160", "3840x2160"], twoMonitorLayout);
 
 /*
 var leftThird = S.op("push", {
