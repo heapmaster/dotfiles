06a60f640c1cd26ff3c9a87893dea304b1dda0f9
heapmaster <heapmaster@gmail.com>
2017-02-22 21:56:31 -0600

    added autocomplete and fonts

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,6 +31,7 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
 
+;; evil mode!!!
 (use-package evil
   :ensure t
   :config
@@ -43,10 +44,12 @@
       "," 'evil-switch-to-windows-last-buffer
       "b" 'helm-mini
       "g" 'magit-status
-    ))
+      "x" 'helm-M-x
+      "o" 'delete-other-windows
+      "t" 'helm-projectile))
+  (evil-define-key 'normal global-map (kbd "C-S-p") 'helm-projectile-switch-project)
   (evil-set-initial-state 'magit-diff-mode 'normal) 
-  ;;(add-to-list 'evil-normal-state-modes '(magit-diff-mode))
-    (evil-mode t))
+  (evil-mode t))
 
 (use-package helm
   :ensure t
@@ -54,6 +57,27 @@
   (helm-mode t)
   (setq helm-buffers-fuzzy-matching t)
   (setq helm-autoresize-mode t))
+
+;; This is auto-complete
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-idle-delay 0.2)
+  (define-key company-active-map [tab] 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+;; Project mode - project level command-t replacement
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+(use-package helm-projectile
+  :ensure t)
 
 ;; themes
 (use-package color-theme-sanityinc-tomorrow :ensure t)
@@ -67,10 +91,6 @@
 (global-linum-mode t)
 (global-hl-line-mode t)
 
-(use-package powerline
-  :ensure t
-  :config
-  (powerline-evil-center-color-theme))
 (use-package powerline-evil
   :ensure t)
 
@@ -93,6 +113,90 @@
 (use-package magit
   :ensure t
   :defer t)
+
+;; Set up font
+(add-hook 'window-setup-hook
+          (lambda ()
+            (add-to-list 'default-frame-alist '(font . "Hack"))
+            (set-face-attribute 'default nil :font "Hack")
+            (set-face-attribute 'default nil :height 100)
+            (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen)))
+
+;; Taken from aaron bieber's dotfiles; really need to change the colors
+(defface my-pl-segment1-active
+  '((t (:foreground "#000000" :background "#E1B61A")))
+  "Powerline first segment active face.")
+(defface my-pl-segment1-inactive
+  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  "Powerline first segment inactive face.")
+(defface my-pl-segment2-active
+  '((t (:foreground "#F5E39F" :background "#8A7119")))
+  "Powerline second segment active face.")
+(defface my-pl-segment2-inactive
+  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  "Powerline second segment inactive face.")
+(defface my-pl-segment3-active
+  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  "Powerline third segment active face.")
+(defface my-pl-segment3-inactive
+  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  "Powerline third segment inactive face.")
+(use-package powerline
+  :ensure t
+  :config
+  ;;(powerline-evil-center-color-theme))
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (seg1 (if active 'my-pl-segment1-active 'my-pl-segment1-inactive))
+                          (seg2 (if active 'my-pl-segment2-active 'my-pl-segment2-inactive))
+                          (seg3 (if active 'my-pl-segment3-active 'my-pl-segment3-inactive))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (let ((evil-face (powerline-evil-face)))
+                                       (if evil-mode
+                                           (powerline-raw (powerline-evil-tag) evil-face)
+                                         ))
+                                     (if evil-mode
+                                         (funcall separator-left (powerline-evil-face) seg1))
+                                     (powerline-buffer-id seg1 'l)
+                                     (powerline-raw "[%*]" seg1 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format seg1 'l))
+                                     (powerline-raw " " seg1)
+                                     (funcall separator-left seg1 seg2)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object seg2 'l))
+                                     (powerline-major-mode seg2 'l)
+                                     (powerline-process seg2)
+                                     (powerline-minor-modes seg2 'l)
+                                     (powerline-narrow seg2 'l)
+                                     (powerline-raw " " seg2)
+                                     (funcall separator-left seg2 seg3)
+                                     (powerline-vc seg3 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) seg3 'l))))
+                          (rhs (list (powerline-raw global-mode-string seg3 'r)
+                                     (funcall separator-right seg3 seg2)
+                                     (unless window-system
+                                       (powerline-raw (char-to-string #xe0a1) seg2 'l))
+                                     (powerline-raw "%4l" seg2 'l)
+                                     (powerline-raw ":" seg2 'l)
+                                     (powerline-raw "%3c" seg2 'r)
+                                     (funcall separator-right seg2 seg1)
+                                     (powerline-raw " " seg1)
+                                     (powerline-raw "%6p" seg1 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud seg1 seg3)))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill seg3 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
 ;; Make tab work right in asm-mode
 (defun my-asm-mode-hook ()
   ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
