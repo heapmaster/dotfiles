474b37688c3a7c555548b721b11793843940b338
snewberry <heapmaster@gmail.com>
2021-12-06 08:20:21 -0600

    brew and os x setup stuff

---
+++ install_scripts/brew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Bashing a curl is naughty, but this is what brew and oh-my-zsh recommend
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew update
+brew bundle
---
+++ install_scripts/download_dmgs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /tmp/
+
+wget https://emacsformacosx.com/emacs-builds/Emacs-27.2-3-universal.dmg
+open Emacs-27.2-3-universal.dmg
